ecf0b0ed838e4aa14901d86d45ac1498cf56cf7d
Madison Dellamea <mdellamea@lcs.on.ca>
2022-05-06 14:27:17 -0400

    Initial Commit

---
+++ FoodCatcher/FoodCatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodCatcherApp.swift
+//  FoodCatcher
+//
+//  Created by Madison Dellamea on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodCatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
